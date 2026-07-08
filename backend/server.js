c22d0ffdfab51a60b51769e8ce4ce225eb419102
Aishwaryabhavaraju <aishwaryabhavaraju11@gmail.com>
2026-07-09 01:04:37 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const server = http.createServer(app);
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 initSocket(server);
 
 server.listen(PORT, () => {
